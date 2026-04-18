07615c73d24b31ac9dab3c0ef49e4d5a8c5d7104
Kazuya Ueoka <info@fromkk.info>
2026-04-18 22:58:03 +0900

    fix summary

--- NagoyaSwiftPackage/Sources/NagoyaSwiftSlides/Slides/13_Summary.swift
+++ NagoyaSwiftPackage/Sources/NagoyaSwiftSlides/Slides/13_Summary.swift
@@ -8,7 +8,10 @@ struct Summary: View {
 
   var script: String = """
     まとめです。
-
+    # Type というアプリを紹介させていただきました。
+    そして Markdown や CommonMark を紹介し、
+    swift-markdown で Block や Inline をハンドリングして UI を作成して、
+    SlideKit に PR を出して事前に生成した UI でもスライド対応できるようにしました。
     以上です。ご清聴ありがとうございました。
     """
 
@@ -17,7 +20,10 @@ struct Summary: View {
       converter.convertPage("""
         # Summary
 
-        
+        - `# Type` というアプリの紹介
+        - Markdown や CommonMark を紹介
+        - swift-markdown で Block や Inline をハンドリングして UI を作成
+        - SlideKit に PR を出して事前に生成した UI でもスライド対応
         """)
     }
   }
