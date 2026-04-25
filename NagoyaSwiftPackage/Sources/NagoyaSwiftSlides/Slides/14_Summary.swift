07a13fabd50804835b2b2ae0af41e6be34a581a7
Kazuya Ueoka <info@fromkk.info>
2026-04-25 13:37:50 +0900

    show QR

--- NagoyaSwiftPackage/Sources/NagoyaSwiftSlides/Slides/14_Summary.swift
+++ NagoyaSwiftPackage/Sources/NagoyaSwiftSlides/Slides/14_Summary.swift
@@ -4,6 +4,8 @@ import SwiftUI
 
 @Slide
 struct Summary: View {
+  @Environment(\.slideTheme) var slideTheme
+
   let converter = MarkdownToSlideConverter()
 
   var script: String = """
@@ -16,15 +18,23 @@ struct Summary: View {
 
   var body: some View {
     SlideWrapper {
-      converter.convertPage(
-        """
-        # Summary
+      HStack {
+        converter.convertPage(
+          """
+          # Summary
 
-        - `# Type` というアプリの紹介
-        - Markdown や CommonMark を紹介
-        - swift-markdown で Block や Inline をハンドリングして UI を作成
-        - SlideKit に PR を出して事前に生成した UI でもスライド対応
-        """)
+          - `# Type` というアプリの紹介
+          - Markdown や CommonMark を紹介
+          - swift-markdown で Block や Inline をハンドリングして UI を作成
+          - SlideKit に PR を出して事前に生成した UI でもスライド対応
+          - 資料置き場: [https://github.com/fromkk/NagoyaSwift-Second](https://github.com/fromkk/NagoyaSwift-Second)
+          """)
+
+        Image(.nagoyaSwiftRepoQr)
+          .resizable()
+          .frame(width: 300, height: 300)
+      }
+      .padding(slideTheme.contentPadding)
     }
   }
 
